661ed9d3943b9e9b14f0ecbd851a1244f822f6f1
Xinhao Yuan <xinhaoyuan@gmail.com>
2018-11-29 17:26:17 -0500

    ra_test repro 1

---
+++ ra_test_f775c65_repro_1/README.md
@@ -0,0 +1,5 @@
+This test reproduces the `undefined_reply` error in ra.
+
+This error happens when a `ra_server` in `await_condition` state (due to missing log entry) receives `request_vote` from a higher term.
+The `ra_server` will buffer the request without the sender info, which is required later. When such information is missing, the node will crash.
+This may not be critical due to the resilience guarantee of Raft protocol, but it weakens the guarantee by increasing crashes. 
---
+++ ra_test_f775c65_repro_1/test/ra_test.erl
@@ -0,0 +1,150 @@
+-module(ra_test).
+
+-ifdef(TEST).
+-compile(export_all).
+
+-include_lib("eunit/include/eunit.hrl").
+
+all_test_() ->
+    {timeout, 10, ?_test( test_entry() )}.
+
+-define(config(Name, Config), proplists:get_value(Name, Config)).
+-define(vcall(E), (io:format(user, "~w: ~s => ~p~n", [?LINE, ??E, E]))).
+
+string_to_term(String) ->
+    {ok, Tokens, _EndLine} = erl_scan:string(String),
+    {ok, AbsForm} = erl_parse:parse_exprs(Tokens),
+    {value, Value, _Bs} = erl_eval:exprs(AbsForm, erl_eval:new_bindings()),
+    Value.
+
+test_entry() ->
+    Config = [ {server_id, {tserver1, node()}}
+             , {uid, <<"node1_uid">>}
+             , {server_id2, {tserver2, node()}}
+             , {uid2, <<"node2_uid">>}
+             , {server_id3, {tserver3, node()}}
+             , {uid3, <<"node3_uid">>}
+             , {server_id4, {tserver4, node()}}
+             , {uid4, <<"node4_uid">>}
+             , {server_id5, {tserver5, node()}}
+             , {uid5, <<"node5_uid">>}
+             , {cluster_name, <<"cluster">>}
+             , {priv_dir, "/tmp/ra"}
+             ],
+
+    ok = application:load(ra),
+    application:ensure_all_started(lg),
+    run_test_fun(Config, inconsistent_state_2),
+    ok.
+
+run_test_fun(Config, FName) ->
+    PrivDir = ?config(priv_dir, Config),
+    case PrivDir of
+        undefined ->
+            io:format(user, "Skip cleanup~n");
+        [] ->
+            io:format(user, "Skip cleanup due to empty priv dir~n");
+        _ ->
+            Cmd = lists:flatten(
+                    io_lib:format(
+                      "rm -rf ~s/*",
+                      [PrivDir])),
+            io:format(user, "Clean up with ~s~n", [Cmd]),
+            os:cmd(Cmd)
+    end,
+
+    ra:start_in(PrivDir),
+    apply(?MODULE, FName, [Config]),
+    application:stop(ra),
+    ok.
+
+inconsistent_state_2(Config) ->
+    ClusterName = ?config(cluster_name, Config),
+    PrivDir = ?config(priv_dir, Config),
+    ServerId1 = ?config(server_id, Config),
+    ServerId2 = ?config(server_id2, Config),
+    ServerId3 = ?config(server_id3, Config),
+    ServerId4 = ?config(server_id4, Config),
+    ServerId5 = ?config(server_id5, Config),
+    Peers = [ServerId1, ServerId2, ServerId3, ServerId4, ServerId5],
+    ok = start_cluster(ClusterName, Peers),
+
+    [ begin
+          UId = ra_directory:uid_of(Name),
+          ?assert(filelib:is_dir(filename:join([ra_env:data_dir(), UId])))
+      end || {Name, _} <- Peers],
+
+    timer:sleep(1000),
+    ra:trigger_election(ServerId5),
+    timer:sleep(1000),
+
+    %% Assume ServerId5 is the leader
+    {ok, undefined, ServerId5} = ra:consistent_query(ServerId5, fun (_) -> undefined end),
+
+    ?vcall(catch ra:stop_server(ServerId3)),
+    ?vcall(catch enqueue(ServerId2, msg1)),
+    ?vcall(catch enqueue(ServerId2, msg2)),
+    timer:sleep(250),
+    %% Bump term
+    ?vcall(catch ra:stop_server(ServerId5)),
+    ?vcall(catch ra:restart_server(ServerId5)),
+    timer:sleep(250),
+    ?vcall(catch ra:restart_server(ServerId3)),
+    timer:sleep(250),
+    %% Now tserver3 should be going to await_condition
+    erlang:suspend_process(whereis(tserver3)),
+    %% Bump term
+    {ok, undefined, Leader2} = ra:consistent_query(ServerId2, fun (_) -> undefined end),
+    ?vcall(catch ra:stop_server(Leader2)),
+    ?vcall(catch ra:restart_server(Leader2)),
+    timer:sleep(250),
+    erlang:resume_process(whereis(tserver3)),
+    timer:sleep(2000),
+    ok.
+
+%% from ra_2_SUITE.erl
+start_cluster(ClusterName, ServerIds, Config) ->
+    {ok, Started, _} = ra:start_cluster(ClusterName,
+                                        {module, ?MODULE, Config},
+                                        ServerIds),
+    ?assertEqual(lists:sort(ServerIds), lists:sort(Started)),
+    ok.
+
+start_cluster(ClusterName, ServerIds) ->
+    start_cluster(ClusterName, ServerIds, #{}).
+
+enqueue(Server, Msg) ->
+    {ok, _, _} = ra:process_command(Server, {enq, Msg}),
+    ok.
+
+dequeue(Server) ->
+    {ok, Res, _} = ra:process_command(Server, deq),
+    Res.
+
+%% ra_machine test impl
+init(_) ->
+    queue:new().
+
+'apply'(_Meta, {enq, Msg}, Effects, State) ->
+    {queue:in(Msg, State), Effects, ok};
+'apply'(_Meta, deq, Effects, State0) ->
+    case queue:out(State0) of
+        {{value, Item}, State} ->
+            {State, Effects, Item};
+        {empty, _} ->
+            {State0, Effects, empty}
+    end;
+'apply'(_Meta, {deq, Pid}, Effects, State0) ->
+    case queue:out(State0) of
+        {{value, Item}, State} ->
+            {State, [{send_msg, Pid, Item, ra_event} | Effects], ok};
+        {empty, _} ->
+            {State0, Effects, ok}
+    end.
+
+state_enter(eol, State) ->
+    [{send_msg, P, eol, ra_event} || {P, _} <- queue:to_list(State), is_pid(P)];
+state_enter(_, _) ->
+    [].
+
+-endif.
