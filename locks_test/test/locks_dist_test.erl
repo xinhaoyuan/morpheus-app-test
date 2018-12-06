005fc41289968545f5497232a1cc92d469e2de7a
Xinhao Yuan <xinhaoyuan@gmail.com>
2018-12-05 23:20:08 -0500

    tweaks

--- locks_test/test/locks_dist_test.erl
+++ locks_test/test/locks_dist_test.erl
@@ -7,36 +7,49 @@
 all_test_() ->
     {timeout, 120, ?_test( test_entry() )}.
 
+-define(config(Key, Data), proplists:get_value(Key, Data)).
+
+try_getenv(Name, Handler, Default) ->
+    case os:getenv(Name) of
+        false ->
+            Default;
+        S -> Handler(S)
+    end.
+
 test_entry() ->
-    Sched =
-        case os:getenv("SCHED") of
-            false ->
-                basicpos;
-            _S -> list_to_atom(_S)
-        end,
-    io:format(user, "Use sched ~s~n", [Sched]),
+    Config =
+        [ {sched, try_getenv("SCHED", fun list_to_atom/1, basicpos)}
+        , {repeat, try_getenv("REPEAT", fun list_to_integer/1, 100)}
+        ],
+    MConfig =
+        [ monitor
+        , { fd_opts
+          , [ { scheduler
+              , {?config(sched, Config), []} }
+            , verbose_final ] }
+        , {node, node1@localhost}
+        , {clock_limit, 600000}
+        , {clock_offset, 1539105131938}
+        , stop_on_deadlock
+          %% , trace_send, trace_receive
+          %% , verbose_handle, verbose_ctl
+          %% , {trace_from_start, true}
+        ]
+        ++ case os:getenv("ONLY_SEND") of
+               false -> [];
+               "" -> [];
+               _ -> [{only_schedule_send, true}]
+           end,
     {Ctl, MRef} = morpheus_sandbox:start(
-                    ?MODULE, t_sandbox_entry, [],
-                    [ monitor
-                    , { fd_opts
-                      , [ { scheduler
-                          , {Sched, []} }
-                        , verbose_final ] }
-                    , {node, node1@localhost}
-                    , {clock_limit, 600000}
-                    , {clock_offset, 1539105131938}
-                    , stop_on_deadlock
-                    %% , trace_send, trace_receive
-                    %% , verbose_handle, verbose_ctl
-                    %% , {trace_from_start, true}
-                    ]),
+                    ?MODULE, t_sandbox_entry, [Config],
+                    MConfig),
     success = receive {'DOWN', MRef, _, _, Reason} -> Reason end,
     ok.
 
 -define(G, morpheus_guest).
 -define(GH, morpheus_guest_helper).
 
-t_sandbox_entry() ->
+t_sandbox_entry(Config) ->
     Nodes = [Node1, Node2, Node3] =
         [node1@localhost, node2@localhost, node3@localhost],
     lists:foreach(fun (Node) ->
@@ -51,7 +64,7 @@ t_sandbox_entry() ->
     ets:insert(tab, {test_counter, 0}),
 
     ?GH:sync_task(
-       [ repeat, 1
+       [ repeat, ?config(repeat, Config)
        , fun () ->
                  Cnt = ets:update_counter(tab, test_counter, 1),
                  io:format(user, "Test ~w~n", [Cnt]),
