6f4f3acd72d74ba04e4b26d6e191f3aa0477954a
Xinhao Yuan <xinhaoyuan@gmail.com>
2019-04-27 09:34:15 -0400

    .

--- run-case.sh
+++ run-case.sh
@@ -2,49 +2,42 @@
 
 output="$1"
 name="$2"
-task_num="$3"
-repeat="$4"
-sched="$5"
-pred="$6"
+case_dir="$3"
+task_num="$4"
+repeat="$5"
+sched="$6"
+pred="$7"
 
 case $name in
     locks-1)
-        case_dir=locks_test
         use_t=t
         testcase=locks_test
         ;;
     locks-2)
-        case_dir=locks_test
         use_t=t
         testcase=locks_dist_test
         ;;
     gproc-1)
-        case_dir=gproc_test
         use_t=f
         testcase=t_simple_reg
         ;;
     gproc-2)
-        case_dir=gproc_test
         use_t=f
         testcase=t_simple_ensure_other
         ;;
     gproc-3)
-        case_dir=gproc_test
         use_t=f
         testcase=t_master_dies
         ;;
     mnesia-1)
-        case_dir=mnesia_test
         use_t=f
         testcase=add_copy_and_restart
         ;;
     mnesia-2)
-        case_dir=mnesia_test
         use_t=f
         testcase=del_copy_and_restart
         ;;
     ms-1)
-        case_dir=rabbit_test
         use_t=t
         testcase=mirrored_supervisor_test
         ;;
@@ -53,20 +46,14 @@ case $name in
         exit 1
 esac
 
-lockfile="dir-lock-${case_dir}_copy_${task_num}"
-if [ -e $lockfile ]; then
-    echo "$lockfile exists. Other parallel test may be running. Be careful and remove the file to continue"
-    exit 1
-fi
-touch "dir-lock-${case_dir}_copy_${task_num}"
-
 ./create_copies.sh $case_dir $task_num $task_num
+truncate -s 0 $output
+echo Redirect output to $output
     
 for X in `seq 1 ${repeat}`; do
     if [ "$use_t" = t ]; then
-        env REPEAT=1 PRED=$pred SCHED=$sched make -C ${case_dir}_copy_${task_num} t=$testcase | tee -a $output
+        env REPEAT=1 PRED=$pred SCHED=$sched make -C ${case_dir}_copy_${task_num} eunit t=$testcase >>$output 2>&1
     else
-        env TESTCASE=$testcase REPEAT=1 PRED=$pred SCHED=$sched make -C ${case_dir}_copy_${task_num} t=$testcase | tee -a $output
+        env TESTCASE=$testcase REPEAT=1 PRED=$pred SCHED=$sched make -C ${case_dir}_copy_${task_num} eunit t=$testcase >>$output 2>&1
     fi
 done
-rm $lockfile
--- run.py
+++ run.py
@@ -6,21 +6,56 @@ import signal
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", type = str, dest = "case", required = True)
-parser.add_argument("-n", type = int, dest = "task_num", default = 1)
+parser.add_argument("-n", type = int, dest = "task_num", default = -10)
 parser.add_argument("--repeat", type = int, dest = "repeat", default = 1000)
 parser.add_argument("--sched", type = str, dest = "sched", required = True)
 parser.add_argument("--pred", type = str, dest = "pred", default = "no")
 args = parser.parse_args()
+
+case_dir = {
+    "locks-1" : "locks_test",
+    "locks-2" : "locks_test",
+    "gproc-1" : "gproc_test",
+    "gproc-2" : "gproc_test",
+    "gproc-3" : "gproc_test",
+    "mnesia-1": "mnesia_test",
+    "mnesia-2": "mnesia_test",
+    "ms-1"    : "rabbit_test",
+}
+
+if args.case not in case_dir:
+    sys.stderr.write("Unknown case {}.\n".format(args.case))
+    sys.exit(1)
+
+if args.task_num < 0:
+    limit = -args.task_num;
+    for t in range(1, limit + 1):
+        lockfile = "lock-{case_dir}-{task_num}".format(case_dir = case_dir[args.case], task_num = t)
+        try:
+            os.open(lockfile, os.O_CREAT | os.O_EXCL)
+            args.task_num = t
+            break
+        except FileExistsError:
+            continue
+    if args.task_num < 0:
+        sys.stderr.write("Cannot find a idle task num\n")
+        sys.exit(1)
+    else:
+        sys.stderr.write("Select task num {}\n".format(args.task_num))
 
 filename = "output-{case}-{task_num}-{repeat}-{sched}".format(
     case = args.case, task_num = args.task_num, repeat = args.repeat, sched = args.sched)
 
 if os.path.isfile(filename):
     sys.stderr.write("Output file [{}] already exists. Consider backing it up.\n".format(filename))
+    os.remove(lockfile)
+    sys.exit(1)
 
-p = sp.Popen(["./run-case.sh", filename, args.case, str(args.task_num), str(args.repeat), args.sched, args.pred])
+p = sp.Popen(["./run-case.sh", filename, args.case, case_dir[args.case], str(args.task_num), str(args.repeat), args.sched, args.pred])
 try:
     p.wait()
 except KeyboardInterrupt:
     sys.stderr.write("Terminating subprocess {}\n".format(p.pid))
     os.kill(p.pid, signal.SIGINT)
+
+os.remove(lockfile)
