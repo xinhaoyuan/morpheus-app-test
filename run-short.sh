f06702636e96e1dd0cb3b467d7f7a25a37a4962e
Xinhao Yuan <xinhaoyuan@gmail.com>
2019-04-27 20:00:43 -0400

    .

--- run-short.sh
+++ run-short.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-for i in `seq 1 1000`; do
+rm $1/acc.dat
+for i in `seq 1 100`; do
     if [ -z "$USE_T" ]; then
-        env PRED=ploc TESTCASE=$2 REPEAT=1 SCHED=simrw make -C $1 eunit | tee -a output-ploc-${2}-simrw.txt
+        env PRED=ploc TESTCASE=$2 REPEAT=1 SCHED=basicpos make -C $1 eunit | tee -a output-ploc-${2}-simrw.txt
     else
-        env PRED=ploc REPEAT=1 SCHED=simrw make -C $1 eunit t=$2 | tee -a output-ploc-${2}-simrw.txt
+        env PRED=ploc REPEAT=1 SCHED=basicpos make -C $1 eunit t=$2 | tee -a output-ploc-${2}-simrw.txt
     fi
 done
